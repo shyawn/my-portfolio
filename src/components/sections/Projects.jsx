2dd92b44df40ed936401c0f810f93f39569813a3
ShawnCsj <csjshawn@gmail.com>
2025-02-17 21:32:20 +0800

    chore: edit project content

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,35 +13,37 @@ export const Projects = () => {
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2">
-                {" "}
-                Conference Mobile Application
-              </h3>
-              <p className="text-gray-400 mb-4">
-                Full-stack platform with modern UI to streamline event
-                management, enhance attendee engagement, and provide real-time
-                updates.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {[
-                  "Vue.js",
-                  "Ionic",
-                  "Node.js",
-                  "AWS S3",
-                  "Docker",
-                  "Nest.js",
-                  "TypeScript",
-                ].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition flex flex-col justify-between">
+              <div>
+                <h3 className="text-xl font-bold mb-2">
+                  {" "}
+                  Conference Mobile Application
+                </h3>
+                <p className="text-gray-400 mb-4">
+                  Full-stack platform with modern UI to streamline event
+                  management, enhance attendee engagement, and provide real-time
+                  updates.
+                </p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {[
+                    "Vue.js",
+                    "Ionic",
+                    "Node.js",
+                    "AWS S3",
+                    "Docker",
+                    "Nest.js",
+                    "TypeScript",
+                  ].map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
                                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
                     "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
 
               <div className="flex justify-between items-center">
@@ -58,71 +60,79 @@ export const Projects = () => {
               glass p-6 rounded-xl border border-white/10 
               hover:-translate-y-1 hover:border-blue-500/30
               hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all
+              transition-all flex flex-col justify-between
             "
             >
-              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
-              <p className="text-gray-400 mb-4">
-                ML-powered data visualization platform with predictive analytics
-                and interactive reports.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="
-                      bg-blue-500/10 text-blue-500 py-1 px-3 
-                      rounded-full text-sm
-                      transition
-                      hover:bg-blue-500/20 hover:-translate-y-0.5
-                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center">
-                <a
-                  href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  View Project →
-                </a>
-              </div>
-            </div>
-
-            <div
-              className="
-              glass p-6 rounded-xl border border-white/10 
-              hover:-translate-y-1 hover:border-blue-500/30
-              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all
-            "
-            >
-              <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
-              <p className="text-gray-400 mb-4">
-                Full-stack e-commerce with modern UI, secure payment
-                integration, and customizable product inventory.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
-                  (tech) => (
+              <div>
+                <h3 className="text-xl font-bold mb-2">
+                  Movie Web Application
+                </h3>
+                <p className="text-gray-400 mb-4">
+                  React frontend platform integrated with real-time API and
+                  Firebase authentication, enabling users to explore and save
+                  favorite movies with a seamless experience.
+                </p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {["React", "API", "Firebase"].map((tech, key) => (
                     <span
-                      key={tech}
+                      key={key}
                       className="
                       bg-blue-500/10 text-blue-500 py-1 px-3 
                       rounded-full text-sm
                       transition
                       hover:bg-blue-500/20 hover:-translate-y-0.5
                       hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
-                    "
+                      "
                     >
                       {tech}
                     </span>
-                  )
-                )}
+                  ))}
+                </div>
               </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="/my-portfolio/movie"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            {/* <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all flex flex-col justify-between
+            "
+            >
+              <div>
+                <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
+                <p className="text-gray-400 mb-4">
+                  Full-stack e-commerce with modern UI, secure payment
+                  integration, and customizable product inventory.
+                </p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
+                    (tech) => (
+                      <span
+                        key={tech}
+                        className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                      >
+                        {tech}
+                      </span>
+                    )
+                  )}
+                </div>
+              </div>
+
               <div className="flex justify-between items-center">
                 <a
                   href="#"
@@ -138,30 +148,35 @@ export const Projects = () => {
               glass p-6 rounded-xl border border-white/10 
               hover:-translate-y-1 hover:border-blue-500/30
               hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all
+              transition-all flex flex-col justify-between
             "
             >
-              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
-              <p className="text-gray-400 mb-4">
-                Scalable chat platform supporting real-time messaging, presence,
-                and group chat features.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="
+              <div>
+                <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+                <p className="text-gray-400 mb-4">
+                  Scalable chat platform supporting real-time messaging,
+                  presence, and group chat features.
+                </p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {["Socket.IO", "Express", "React", "Redis"].map(
+                    (tech, key) => (
+                      <span
+                        key={key}
+                        className="
                       bg-blue-500/10 text-blue-500 py-1 px-3 
                       rounded-full text-sm
                       transition
                       hover:bg-blue-500/20 hover:-translate-y-0.5
                       hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
                     "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                      >
+                        {tech}
+                      </span>
+                    )
+                  )}
+                </div>
               </div>
+
               <div className="flex justify-between items-center ">
                 <a
                   href="#"
@@ -170,7 +185,7 @@ export const Projects = () => {
                   View Project →
                 </a>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </RevealOnScroll>
